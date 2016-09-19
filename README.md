84811f406d50f161f34ab974331f6bc598e36ccc
dong11 <956176457@qq.com>
2016-09-19 17:35:16 +0800

    express的使用

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Node.js
 Node.js基础学习
 1.express学习
+    创建项目目录:express -e demo
+    进入项目目录，下载依赖库，构建项目:cd demo && npm install
+    启动项目:npm start
+    打开浏览器访问:http://localhost:3000/
+  express项目结构:
+    bin: 存放启动项目的脚本文件
+    node_modules: 存放所有的项目依赖库。
+    public: 静态文件(css,js,img)
+    routes: 路由文件(MVC中的C,controller)
+    views: 页面文件(Ejs模板)
+    package.json: 项目依赖配置及开发者信息
+    app.js: 应用核心配置文件
 
   
---
+++ demo/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login', {title:'用户登陆'});
+});
+
+router.post('/login', function (req, res, next) {
+  var user = {
+    username:'admin',
+    password:'admin'
+  }
+  if(req.body.username === user.username && req.body.password === user.password){
+    //req.session.user = user;
+    return res.redirect('/home');
+  } else {
+    //req.session.error = '用户名或密码不正确';
+    return res.redirect('/login');
+  }
+});
+
+router.get('/home', function (req, res, next) {
+  res.render('home', { title: 'Home'});
+});
+
+router.get('/logout', function (req, res, next) {
+  //req.session.user = null;
+  res.redirect('/');
+});
+
+module.exports = router;
